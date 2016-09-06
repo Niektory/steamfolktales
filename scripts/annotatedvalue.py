b798dc6a77f7ced1049921b081612cf6b0bc92c3
Niektóry <niektory@gmail.com>
2016-09-06 15:00:50 +0200

    Add multiplication and division operations to AnnotatedExpression

--- scripts/annotatedvalue.py
+++ scripts/annotatedvalue.py
@@ -1,19 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Tomasz "Niektóry" Turowski
 
+from __future__ import division
 
-class AnnotatedValue(object):
-	def __init__(self, value, annotation=None):
-		self.value = value
-		self.annotation = annotation
 
+class AnnotatedValueBase(object):
 	def __int__(self):
 		return int(self.value)
-
-	def __str__(self):
-		return "{}{}".format(
-			self.value,
-			" ({})".format(self.annotation) if self.annotation else "")
 
 	def __add__(self, other):
 		return AnnotatedExpression(self, "+", other)
@@ -27,8 +20,37 @@ class AnnotatedValue(object):
 	def __rsub__(self, other):
 		return AnnotatedExpression(other, "-", self)
 
+	def __mul__(self, other):
+		return AnnotatedExpression(self, "*", other)
 
-class AnnotatedExpression(object):
+	def __rmul__(self, other):
+		return AnnotatedExpression(other, "*", self)
+
+	def __floordiv__(self, other):
+		return AnnotatedExpression(self, "//", other)
+
+	def __rfloordiv__(self, other):
+		return AnnotatedExpression(other, "//", self)
+
+	def __truediv__(self, other):
+		return AnnotatedExpression(self, "/", other)
+
+	def __rtruediv__(self, other):
+		return AnnotatedExpression(other, "/", self)
+
+
+class AnnotatedValue(AnnotatedValueBase):
+	def __init__(self, value, annotation=None):
+		self.value = value
+		self.annotation = annotation
+
+	def __str__(self):
+		return "{}{}".format(
+			self.value,
+			" ({})".format(self.annotation) if self.annotation else "")
+
+
+class AnnotatedExpression(AnnotatedValueBase):
 	def __init__(self, lvalue, operation, rvalue):
 		self.lvalue = lvalue
 		self.operation = operation
@@ -39,38 +61,37 @@ class AnnotatedExpression(object):
 		lvalue = getattr(self.lvalue, "value", self.lvalue)
 		rvalue = getattr(self.rvalue, "value", self.rvalue)
 		if self.operation == "+":
-			return int(lvalue) + int(rvalue)
+			return lvalue + rvalue
 		if self.operation == "-":
-			return int(lvalue) - int(rvalue)
-
-	def __int__(self):
-		return int(self.value)
+			return lvalue - rvalue
+		if self.operation == "*":
+			return lvalue * rvalue
+		if self.operation == "/":
+			return lvalue / rvalue
+		if self.operation == "//":
+			return lvalue // rvalue
 
 	def formatted(self, multiline=False, result=False):
-		need_parentheses = self.operation == "-" and getattr(self.rvalue, "operation", None) == "-"
+		l_need_parentheses = (
+			self.operation in ("*", "/", "//")
+			and getattr(self.lvalue, "operation", None) in ("+", "-"))
+		r_need_parentheses = (
+			(self.operation == "-" and getattr(self.rvalue, "operation", None) == "-")
+			or (self.operation in ("*", "/", "//")
+				and getattr(self.rvalue, "operation", None) in ("+", "-"))
+			or (self.operation in ("/", "//")
+				and getattr(self.rvalue, "operation", None) in ("*", "/", "//")))
 		lvalue = formatted(self.lvalue, multiline=multiline, result=False)
 		rvalue = formatted(self.rvalue, multiline=multiline, result=False)
 		return "{lvalue}{separator}{operator} {rvalue}{result}".format(
-			lvalue=lvalue,
+			lvalue="({})".format(lvalue) if l_need_parentheses else lvalue,
 			separator="\n" if multiline else " ",
 			operator=self.operation,
-			rvalue="({})".format(rvalue) if need_parentheses else rvalue,
+			rvalue="({})".format(rvalue) if r_need_parentheses else rvalue,
 			result="{}= {}".format("\n---\n" if multiline else " ", self.value) if result else "")
 
 	def __str__(self):
 		return self.formatted()
-
-	def __add__(self, other):
-		return AnnotatedExpression(self, "+", other)
-
-	def __radd__(self, other):
-		return AnnotatedExpression(other, "+", self)
-
-	def __sub__(self, other):
-		return AnnotatedExpression(self, "-", other)
-
-	def __rsub__(self, other):
-		return AnnotatedExpression(other, "-", self)
 
 
 def formatted(value, multiline=False, result=False):
