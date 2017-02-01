b013f7d224322d75aaf34d4a02df47e979ee36e3
Niektóry <niektory@gmail.com>
2017-02-01 04:06:02 +0100

    Rewrite generate_xmls.py
    - use ElementTree to construct the XML file to ensure a valid XML
    - update to the new FIFE format
    
    Note that it now needs to be copied together with scripts/xmlhelper.py to work

--- tools/generate_xmls.py
+++ tools/generate_xmls.py
@@ -2,14 +2,38 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Tomasz "Niektóry" Turowski
 
+# copy this file (and scripts/xmlhelper.py) to a directory with images
+# and run it there to generate default object XML files tor them
+# doesn't overwrite existing XML files
+
+from __future__ import print_function
+
 from os import listdir
 from os.path import isfile, splitext
+import xml.etree.ElementTree as ET
+
+from xmlhelper import indent
 
 files = [ f for f in listdir('.') if isfile(f) ]
 for file in files:
-	if splitext(file)[1].lower() in ('.png', '.jpg', '.jpeg'):
-		if not isfile(splitext(file)[0] + '.xml'):
-			print 'generating xml for image:', file
-			with open(splitext(file)[0] + '.xml', 'w') as output:
-				output.write('<?fife type="object"?>\n<object id="' + splitext(file)[0] + '" namespace="steamfolktales" blocking="0" static="1">\n	<image source="' + file + '" direction="0" x_offset="0" y_offset="0" />\n</object>')
-				
+	if splitext(file)[1].lower() not in ('.png', '.jpg', '.jpeg'):
+		continue
+	if isfile(splitext(file)[0] + '.xml'):
+		continue
+	print('generating xml for image:', file)
+	root = ET.Element("assets")
+	tree = ET.ElementTree(element=root)
+	object_element = ET.Element("object")
+	object_element.set("id", splitext(file)[0])
+	object_element.set("namespace", "steamfolktales")
+	object_element.set("blocking", "0")
+	object_element.set("static", "1")
+	root.append(object_element)
+	image_element = ET.Element("image")
+	image_element.set("source", file)
+	image_element.set("direction", "0")
+	image_element.set("x_offset", "0")
+	image_element.set("y_offset", "0")
+	object_element.append(image_element)
+	indent(root)
+	tree.write(splitext(file)[0] + '.xml', encoding="UTF-8", xml_declaration=True)
